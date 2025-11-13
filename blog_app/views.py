5df0a39a5f088e945a0ce465fd17c04daf1692a2
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2025-11-13 10:10:50 +0545

    Added draft detail

---
+++ blog_app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+from blog_app.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_at__isnull=False)
+    return render(
+        request,
+        "post_list.html",
+        {"posts": posts},
+    )
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(
+        request,
+        "post_detail.html",
+        {"post": post},
+    )
+
+
+def draft_list(request):
+    posts = Post.objects.filter(published_at__isnull=True)
+    return render(
+        request,
+        "draft_list.html",
+        {"posts": posts},
+    )
+
+def draft_detail(request, pk):
+    post = Post.objects.filter(pk=pk, published_at__isnull=True)
+    return render(
+        request,
+        "draft_detail.html",
+        {"post": post},
+    )
